c8cd5b0ae31a49539c9e1f21335e3f9700589f71
vividege <xiaohailin136@gmail.com>
2023-05-26 10:20:16 +0800

    Add googletest code

---
+++ test/studentTest.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include "../include/student.h"
+
+// class StudentTests : public testing::Test
+// {
+// public:
+//     Student *s;
+//     virtual void SetUp()
+//     {
+//         s = new Student(34,"Hailin","123456");
+//         cout << "Create a student: [" << s->m_StuID<< " : " << s->m_Name << " ]" << endl;
+//     }
+
+//     virtual void TearDown()
+//     {
+//         cout << "Delete a student: ["  << s->m_StuID<< " : " << s->m_Name << "  ]" << endl;
+//           delete s;
+//     }
+// };
+
+TEST(StudentTests, Get)
+{
+    // EXPECT_EQ("Hailin", s->m_Name);
+    EXPECT_EQ(34, 34);
+    // EXPECT_EQ("123456", s->m_Password);
+}
+
+//TEST_F(StudentTests, set)
+// {
+//     s->m_Name = "Huahua";
+//     s->m_Password = "999999";
+//     s->m_StuID = 123;
+//     EXPECT_EQ("Huahua", s->m_Name);
+//     EXPECT_EQ(79, s->m_StuID);
+//     EXPECT_EQ("999999", s->m_Password);
+// }
